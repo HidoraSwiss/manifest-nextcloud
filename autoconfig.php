72ec6078fdb20c2a6797b1abb86f406a1b865f26
Dubouchj <33114629+Dubouchj@users.noreply.github.com>
2018-10-23 10:08:03 +0200

    Create autoconfig.php

---
+++ autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "nextcloud",
+  "dbuser"        => "username",
+  "dbpass"        => "password",
+  "dbhost"        => "localhost",
+  "dbtableprefix" => "",
+  "adminlogin"    => "root",
+  "adminpass"     => "root-password",
+  "directory"     => "/www/htdocs/nextcloud/data",
+);
