7bf356682a26d1f10f0f4f47ec8addf6a6df5156
Dubouchj <33114629+Dubouchj@users.noreply.github.com>
2020-05-25 11:14:31 +0200

    Create autoconfig.php

---
+++ autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "pgsql",
+  "dbname"        => "DB_NAME",
+  "dbuser"        => "DB_USER",
+  "dbpass"        => "DB_PASSWORD",
+  "dbhost"        => "sqldb",
+  "dbtableprefix" => "",
+  "adminlogin"    => "ADMIN_NAME",
+  "adminpass"     => "ADMIN_PASSWORD",
+  "directory"     => "/var/www/html/data",
+);
